b41839c43fb718c8a64b0e23eb61d5a9f9a87266
Diego Cortes <diegocortes4@gmail.com>
2021-01-31 17:38:41 -0500

    added about

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import react from 'react';
+
+
+function About() {
+  return (
+      <div>
+            <p>My name is Diego Cortes. I am a Full-stack developer student at a Georgia Tech Bootcamp. </p>
+            <p>I am from Bogota, Colombia where I grew up and studied computer engineering at <a href="https://en.wikipedia.org/wiki/Pontifical_Xavierian_University">Pontifical Xavierian University.</a></p>
+            <p> I am a FAA certified remote pilot, and in my free time, I fly drones and enjoy editing videos.</p>
+            <p>Contact Me by:</p>
+            <p>Phone Number: (404)-552-5438</p>
+            <p>Email: diegocortes4@gmail.com</p>
+            <p> GitHub: <a href="https://github.com/diegocortes4">GitHub Profile</a></p>
+        </div>
+            )}
+            export default About;
