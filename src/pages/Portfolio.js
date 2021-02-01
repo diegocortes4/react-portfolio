dfe7de1e041773ab234cddc560b526f0a9591976
Diego Cortes <diegocortes4@gmail.com>
2021-02-01 17:15:34 -0500

    added portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,2 +1,48 @@
 import react from 'react';
 
+
+function About() {
+    return (
+        <div>
+    
+
+ 
+        <section class="row row-cols-1 row-cols-lg-2">
+          <figure class="col">
+            <img src="Assets/freefromall.png" alt="Free-From-All Main Page" width="200">
+            <h5 class="portfolio-details">Free-From-All Makeup Source</h5>
+            <p class="portfolio-details"><a href="https://diegocortes4.github.io/free-from-all/">Website Link</a></p>
+            <p class="portfolio-details"><a href="https://github.com/diegocortes4/free-from-all">GitHub Repository</a></p>
+          </figure>
+
+          <section class="row row-cols-1 row-cols-lg-2">
+            <figure class="col">
+              <img src="Assets/6.png" alt="node-express-handlebars" width="200">
+              <h5 class="portfolio-details">node-express-handlebars</h5>
+              <p class="portfolio-details"><a href="https://handlebars-node-express.herokuapp.com/">Website Link</a></p>
+              <p class="portfolio-details"><a href="https://github.com/diegocortes4/node-express-handlebars">GitHub Repository</a></p>
+            </figure>
+
+
+          <section class="row row-cols-1 row-cols-lg-2">
+            <figure class="col">
+              <img src="Assets/7.png" alt="budget-tracker" width="200">
+              <h5 class="portfolio-details">budget-tracker</h5>
+              <p class="portfolio-details"><a href="https://radiant-crag-82532.herokuapp.com//">Website Link</a></p>
+              <p class="portfolio-details"><a href="https://github.com/diegocortes4/budget-tracker">GitHub Repository</a></p>
+            </figure>
+
+
+          <section class="row row-cols-1 row-cols-lg-2">
+            <figure class="col">
+              <img src="Assets/8.png" alt="cyber santa" width="200">
+              <h5 class="portfolio-details">Cyber Santa Lists</h5>
+              <p class="portfolio-details"><a href="https://project-2-cyber-santa.herokuapp.com/">Website Link</a></p>
+              <p class="portfolio-details"><a href="https://github.com/diegocortes4/Cyber-Santa-lists">GitHub Repository</a></p>
+            </figure>
+
+      </ul>
+   
+</div>
+)}
+export default About;
