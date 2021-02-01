575f5d9b9a74389725f6537d7a8b1419ae7bb04d
Diego Cortes <diegocortes4@gmail.com>
2021-02-01 12:39:39 -0500

    edited routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import About from './pages/About';
-import About from './pages/Portfolio';
 import React from "react";
 import {
   BrowserRouter as Router,
@@ -23,7 +22,7 @@ function App() {
               <Link to="/about">About</Link>
             </li>
             <li>
-              <Link to="/portfolio">Portfolio</Link>
+              <Link to="/Portfolio">Portfolio</Link>
             </li>
           </ul>
         </nav>
@@ -34,7 +33,7 @@ function App() {
           <Route path="/about">
             <About />
           </Route>
-          <Route path="/portfolio">
+          <Route path="/Portfolio">
             <Portfolio />
           </Route>
           <Route path="/">
@@ -47,24 +46,27 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
        
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header> */}
+        </a> */}
+      {/* </header> */}
     </div>
   );
 }
 
+
 function Home() {
   return <h2>Home</h2>;
 }
+
+
 
 
 function Portfolio() {
